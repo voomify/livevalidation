0d5ce8a7abac65820ee9203ba49da67052d84f06
rx <russell@voomify.com>
2010-03-30 16:41:50 -0400

    Added support for :index to the form field helper.  This allows this to be used on a form that has multiple duplicate items.

--- lib/form_helpers.rb
+++ lib/form_helpers.rb
@@ -1,8 +1,8 @@
 module ActionView
-  
+
   mattr_accessor :live_validations
   ActionView::live_validations = true
-  
+
   module Helpers
     module FormHelper
       [ :text_field, :text_area, :password_field ].each do |field_type|
@@ -10,20 +10,27 @@ module ActionView
           live = options.delete(:live)
           live = ActionView::live_validations if live.nil?
           send("#{field_type}_without_live_validations", object_name, method, options) +
-          ( live ? live_validations_for(object_name, method) : '' )
+          ( live ? live_validations_for(object_name, method, options) : '' )
         end
         alias_method_chain field_type, :live_validations
       end
 
-      def live_validations_for(object_name, method)
-        script_tags(live_validation(object_name, method))
+      def live_validations_for(object_name, method, options)
+        script_tags(live_validation(object_name, method, options))
       end
-      
+
       private
-      
-      def live_validation(object_name, method)
+
+      def build_field_name(object_name, method, options)
+          field_name = "#{object_name}_"
+          # if we have multiple items on the same form using index
+          field_name  << "#{options[:index]}_" if options[:index]
+          field_name  << "#{method}"
+      end
+
+      def live_validation(object_name, method, options)
         if validations = self.instance_variable_get("@#{object_name.to_s}").class.live_validations[method.to_sym] rescue false
-          field_name = "#{object_name}_#{method}"
+          field_name = build_field_name(object_name, method, options)
           initialize_validator(field_name) +
           validations.map do |type, configuration|
             live_validation_code(field_name, type, configuration)
@@ -32,15 +39,15 @@ module ActionView
           ''
         end
       end
-      
+
       def initialize_validator(field_name)
         "var #{field_name} = new LiveValidation('#{field_name}');"
       end
-      
+
       def live_validation_code(field_name, type, configuration)
         "#{field_name}.add(#{ActiveRecord::Validations::VALIDATION_METHODS[type]}" + ( configuration ? ", #{configuration.to_json}" : '') + ')'
       end
-      
+
       def script_tags(js_code = '')
          ( js_code.blank? ? '' : "<script>#{js_code}</script>" )
       end
